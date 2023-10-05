60bbf420d605c817f7c6c0aaa9632928a1d76716
5jisoo <56earls@gmail.com>
2023-10-05 21:39:48 +0900

    Feat: initial project

---
+++ market-capitalization/src/test/java/com/shinhansec/marketcapitalization/MarketCapitalizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhansec.marketcapitalization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketCapitalizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
