1c22d495ccc0b221ddc1f8f476c9a1b7ed5ec45e
5jisoo <56earls@gmail.com>
2023-10-06 12:50:15 +0900

    Feat: Portfolio MVC

---
+++ market-capitalization/src/main/java/com/shinhansec/marketcapitalization/portfolio/domain/Portfolio.java
@@ -0,0 +1,30 @@
+package com.shinhansec.marketcapitalization.portfolio.domain;
+
+import com.shinhansec.marketcapitalization.common.BaseEntity;
+import com.shinhansec.marketcapitalization.meeting.domain.Meeting;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import static jakarta.persistence.FetchType.*;
+import static jakarta.persistence.GenerationType.*;
+
+@Entity
+@Getter
+public class Portfolio extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "portfolio_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "meeting_id")
+    private Meeting meeting;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "stock_id")
+    private Stock stock;
+
+    private Long holdingQuantity;
+    private Long averagePurchasePrice;
+}
